3b885b9d1bb35f7d99538a8447621326860f4eed
songsaemi <songsaemiye@naver.com>
2022-02-21 11:14:41 +0900

    첫 수업 HelloJava 출력해보기

---
+++ JavaStudy_7533/src/a01_HelloJava/HellloJava.java
@@ -0,0 +1,54 @@
+package a01_HelloJava;
+
+/**
+ * 
+ * 여러줄 주석 (클래스 또는 메소드를 설명하는 주석)
+ *
+ */
+
+/*
+ * 여러줄 주석(여러줄 메모용)
+ */
+
+//한줄 주석
+
+//주석의 단축키 ctrl+ /
+
+public class HellloJava { //여기는 HelloJava 선언부 입니다.
+	
+		/**
+		 * 
+		 * 프로그램의 시작지점
+		 */
+	
+
+	public static void main(String[] args) {
+		
+//		'문자' => 'ㅁ'
+//		"문자열" => "ㅁㅁㅁㅁㅁㅁ" hello, java
+//		프로그램 실행(Run) 단축키 Ctrl + 11
+		
+		System.out.println("hello, java");
+		
+		System.out.println("송새미");
+		
+		System.out.println(); //또는 /n
+		
+		System.out.print("이름 : ");
+		System.out.println("송새미");
+		System.out.print("연락처 : ");
+		System.out.println("010-3382-7533");
+		System.out.print("주소 : ");
+		System.out.println("부산 북구 화명동");
+		
+		
+		/*
+		 * 이름 : 송새미
+		 * 연락처: 010-3382-7533
+		 * 주소: 부산 북구 화명동
+		 */
+
+
+	}
+
+}
