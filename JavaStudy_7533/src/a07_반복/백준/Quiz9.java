ca115a6571b9e27f9558633782b0e0540f8b9881
songsaemi <songsaemiye@naver.com>
2022-03-03 09:01:53 +0900

    반복 수업내용 추가

---
+++ JavaStudy_7533/src/a07_반복/백준/Quiz9.java
@@ -0,0 +1,38 @@
+package a07_반복.백준;
+
+import java.util.Scanner;
+
+public class Quiz9 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner =new Scanner(System.in);	
+		int n = 0;
+		
+		System.out.print("반복 횟수 입력: ");
+		n = scanner.nextInt();
+		for(int i =0; i < n; i++) {
+		for(int j = 0; j < n-i-1; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0;  j < (i * 2) + 1 ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i =0; i < n; i++) {
+			for(int j = 0; j < i+1; j++) {
+					System.out.print(" ");
+				}
+				for(int j = 0;  j < (i * 2) + 1 ; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+		
+		
+			
+		
+		
+}
+}}
