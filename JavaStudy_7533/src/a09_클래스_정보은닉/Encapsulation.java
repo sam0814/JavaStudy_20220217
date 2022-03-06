31a674361cae2345044e6e884d89fdd77541485c
songsaemi <songsaemiye@naver.com>
2022-03-07 08:59:53 +0900

    클래스 정보은닉 수업 내용

--- JavaStudy_7533/src/a09_클래스_정보은닉/Encapsulation.java
+++ JavaStudy_7533/src/a09_클래스_정보은닉/Encapsulation.java
@@ -29,10 +29,30 @@ public class Encapsulation {
 		return username;
 	}
 	
+	// getter
+	// public
+	
+	
 	public void showInfo() {
 		System.out.println(username);
 		System.out.println(password);
 		System.out.println(provider);
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
 	}
 
 }
