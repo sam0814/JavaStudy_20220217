d146e963302ca71eb81fb1ba8c6368c1a310ddf4
songsaemi <songsaemiye@naver.com>
2022-03-09 13:00:19 +0900

    상속 수업 내용 추가

---
+++ JavaStudy_7533/src/a11_상속2/Company.java
@@ -0,0 +1,27 @@
+package a11_상속2;
+
+public class Company {
+	private String companyName;
+	public Company() {
+		// TODO Auto-generated constructor stub
+	}
+	public Company(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	
+	public String getCompanyName() {
+		return companyName;
+	}
+
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+
+	public void showCompanyInfo() {
+		System.out.println("회사명: " + companyName);
+	}
+
+}
---
+++ JavaStudy_7533/src/a11_상속2/CompanyTest.java
@@ -0,0 +1,42 @@
+package a11_상속2;
+
+public class CompanyTest {
+
+	public static void main(String[] args) {
+		ElectronicsCompany samsung = new ElectronicsCompany("삼성");
+		ElectronicsCompany lg = new ElectronicsCompany("LG");
+		DeliveryCompany baemin = new DeliveryCompany("배달의 민족");
+		DeliveryCompany yogiyo = new DeliveryCompany("요기요");
+		
+		samsung.setCompanyName("삼숭");
+		
+		Company samsung_company = samsung;
+		Company lg_company = lg ;
+		Company  baemin_company = baemin;
+		Company yogiyo_company = yogiyo;
+		
+		samsung_company.setCompanyName("삼성");
+		
+		
+		//samsung.setCompanyName("삼성전자");
+		//lg.setCompanyName("LG");
+		//baemin.setCompanyName("배달의 민족");
+		//yogiyo.setCompanyName("요기요");
+		
+		samsung.showCompanyInfo();
+		lg.showCompanyInfo();
+		baemin.showCompanyInfo();
+		yogiyo.showCompanyInfo();
+		
+		samsung.addFactory();
+		lg.addFactory();
+		baemin.addMotocycle();
+		yogiyo.addMotocycle();
+		
+		samsung_company.showCompanyInfo();
+		
+		ElectronicsCompany down_samsung_Comapny = (ElectronicsCompany)samsung_company;
+		down_samsung_Comapny.addFactory();
+	}
+
+}
