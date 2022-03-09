49b3560620b2e491ef03d9a5b5edfc60fc252a3d
songsaemi <songsaemiye@naver.com>
2022-03-09 13:01:41 +0900

    인터페이스 수업 내용

---
+++ JavaStudy_7533/src/a13_인터페이스/VolumeDownButton.java
@@ -0,0 +1,7 @@
+package a13_인터페이스;
+
+public interface VolumeDownButton {
+
+	public void keepTheDownButton();
+
+}
---
+++ JavaStudy_7533/src/a13_인터페이스/VolumeUpButton.java
@@ -0,0 +1,8 @@
+package a13_인터페이스;
+
+public interface VolumeUpButton {
+
+	public void keepTheUpButton();
+	
+
+}
